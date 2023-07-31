9207b802e07e28a9ae1a85e72c4d78695b0659f3
xakaz <loic.hernandez@sfr.fr>
2023-07-31 11:42:52 +0200

    create input

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Input from "./Components/Input";
 
 function App() {
-  return (
-    <div className="bg-slate-900 h-screen w-screen">
 
-    </div>
-  );
+
+  return(
+  <div className="bg-slate-900 h-screen flex justify-center items-center">
+    <Input/>
+  </div>
+  )
 }
 
 export default App;
---
+++ src/Components/Button.js
@@ -0,0 +1,17 @@
+function Button(props) {
+  return (
+    <button
+      type="submit"
+      className={
+        "px-4 py-2 font-medium tracking-wide w-fit rounded cursor-pointer " +
+        props.textColor +
+        " " +
+        props.bgColor
+      }
+    >
+      {props.name}
+    </button>
+  );
+}
+
+export default Button;
